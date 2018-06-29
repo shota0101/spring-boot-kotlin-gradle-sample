1c51cf6b059a7b281ce5bea9a7a336fa7bc4893d
Shota Hayashi <qsezy1@hotmail.com>
2018-06-30 01:46:38 +0900

    コメント追加

--- src/main/kotlin/com/example/springbootkotlingradlesample/DemoController.kt
+++ src/main/kotlin/com/example/springbootkotlingradlesample/DemoController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
+// http://localhost:8080/hello?param=test
 @RestController
 class DemoController {
 
