406dbf0194d0cf5ccc8f0895092b42ac9bd85729
Shota Hayashi <qsezy1@hotmail.com>
2018-06-30 01:38:29 +0900

    init

---
+++ src/main/kotlin/com/example/springbootkotlingradlesample/SpringBootKotlinGradleSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootkotlingradlesample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinGradleSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinGradleSampleApplication>(*args)
+}
