e06f90309aa210bb3005c5fdab27906aeb73381b
Shota Hayashi <qsezy1@hotmail.com>
2018-06-30 01:46:17 +0900

    パラメータを受け取るように修正

--- src/main/kotlin/com/example/springbootkotlingradlesample/DemoController.kt
+++ src/main/kotlin/com/example/springbootkotlingradlesample/DemoController.kt
@@ -3,6 +3,7 @@ package com.example.springbootkotlingradlesample
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
@@ -10,9 +11,13 @@ import java.util.*
 class DemoController {
 
     @RequestMapping("/hello")
-    fun hello(): ResponseEntity<Map<String, String>> {
+    fun hello(
+            @RequestParam(value = "param", required = false, defaultValue = "default") param: String
+    ): ResponseEntity<Map<String, String>> {
+
         val data: Map<String, String> = mapOf(
                 "message" to "Hello, Kotlin in Spring Boot",
+                "param" to param,
                 "date" to Date().toString())
 
         val responce: ResponseEntity<Map<String, String>>
