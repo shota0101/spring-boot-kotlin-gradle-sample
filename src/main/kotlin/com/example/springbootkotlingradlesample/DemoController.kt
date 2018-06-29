9b726617f74eee7aa470fddc5cf0b858059320f2
Shota Hayashi <qsezy1@hotmail.com>
2018-06-30 01:44:49 +0900

    レスポンスをJSON形式で返却

--- src/main/kotlin/com/example/springbootkotlingradlesample/DemoController.kt
+++ src/main/kotlin/com/example/springbootkotlingradlesample/DemoController.kt
@@ -1,13 +1,22 @@
 package com.example.springbootkotlingradlesample
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class DemoController {
 
     @RequestMapping("/hello")
-    fun hello(): String {
-        return "Hello, Kotlin in Spring Boot"
+    fun hello(): ResponseEntity<Map<String, String>> {
+        val data: Map<String, String> = mapOf(
+                "message" to "Hello, Kotlin in Spring Boot",
+                "date" to Date().toString())
+
+        val responce: ResponseEntity<Map<String, String>>
+                = ResponseEntity(data, HttpStatus.OK)
+        return responce
     }
 }
